4013615e08e646ecf7ad64c5cef3a4f4dbec790a
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-10-01 21:42:37 +0300

    vector_is_progress

--- SequenceContainers/main.cpp
+++ SequenceContainers/main.cpp
@@ -1,10 +1,12 @@
 ﻿#include<iostream>
 #include<array>
+#include<vector>
 #include<stdexcept>
 #include <fenv.h> 
 #include<cmath>
 
 #define tab "\t"
+#define DILIMITER "\n===============================\n"
 //using namespace std;
 using std::cout;
 using std::cin;
@@ -34,9 +36,11 @@ using std::endl;
 		}
 	}
 
+	template<typename T>void vector_info(const std::vector<T>& vec);
 
 //#define STL_ARRAY
-#define STL_EXCEPTION_HW
+#define STL_VECTOR
+//#define STL_EXCEPTION_HW
 
 void main()
 {
@@ -75,7 +79,40 @@ void main()
 	//std::string 
 	
 #endif // STL_ARRAY
+#ifdef STL_VECTOR
+	std::vector<int> vec = {0,1,1,2,3,5,8,13,21,34};
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	vector_info(vec);
+	cout << endl;
+	vec.push_back(55);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+		cout << *it << tab;
+	vector_info(vec);
+	cout << endl;
+	for(std::vector<int>::reverse_iterator it=vec.rbegin();it!=vec.rend(); ++it)
+		cout << *it << tab;
+	vector_info(vec);
+	cout << endl;
+	cout << "shrink_to_fit" << endl;
+	//vec.shrink_to_fit();
+	/*vector_info(vec);
+	vec.reserve(1600);
+	vec.push_back(1024);*/
+	vec.resize(18);
+	for (int i : vec)cout << i << tab;
+	vector_info(vec);
+	cout << "insert" << endl;
+	int index;
+	int value;
+	cout << "Enter index: "; cin >> index;
+	cout << "Enter value: "; cin >> value;
+	vec.insert(vec.begin() + index, value);
+	for (int i : vec)cout << i << tab;
 
+#endif // STL_VECTOR
 #ifdef STL_EXCEPTION_HW
 
 		int n;
@@ -84,6 +121,19 @@ void main()
 
 #endif // STL_EXCEPTION_HW
 
-
-
+}
+template<typename T>void vector_info(const std::vector<T>& vec)
+{
+	cout << endl;
+	cout << "Size:\t" << vec.size() << endl;//фактический размер Вектора, сколько элементов хранит вектор
+	cout << "Max_Size:\t" << vec.max_size() << endl;
+	cout << "Capacity:\t" << vec.capacity() << endl;//Объем зарезервированной памяти. 
+	// Зарезервированная память нужна для того чтобы ускорить процесс добавления элементов 
+	// и избежать необходимости переопределять память при каждом добавленни элемента -
+	// Это экономит ресурсы процессорного времени, но расходует дополнительные ресурсы памяти.
+	// Если у вектора есть свободная зарезервированная память, то добавляемое значение просто записывается в конец массива,
+	// но если зарезервированная память закончилась, то при добавлении элемента вектор резервирует новую память
+	// в половину больше фактического размера(в 1.5 раза больше фактического размера)
+	// На CAPACITY можно повлиять при помощи методов reserve(), resize(), shrink_to_fit().
+	cout << DILIMITER << endl;
 }
