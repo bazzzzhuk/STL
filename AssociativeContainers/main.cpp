8df71a9facb64bf789c682c646d0da7f26c7dfe3
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-10-03 21:29:16 +0300

    map in progress

---
+++ AssociativeContainers/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<map>
+#include<list>
+//using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+#define DELIMITER "\n================\n"
+
+#define STL_MAP
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STL_MAP
+	std::map<int, std::string> week =
+	{
+		std::pair<int, std::string>(0, "Воскресенье"),
+		std::pair<int, std::string>(1, "Понедельник"),
+		std::pair<int, std::string>(2, "Вторник"),
+		std::pair<int, std::string>(3, "Среда"),
+		{4,"Четверг"},
+		{5,"Пятница"},
+		{6,"Суббота"},
+		{7,"Воскресенье"}
+	};
+	for(std::map<int, std::string>::iterator it = week.begin(); it != week.end(); ++it)
+	{
+		cout << it->first << tab << it->second << endl;
+	}
+	cout << DELIMITER;
+	for (std::pair<int, std::string> i : week)
+		cout << i.first << tab << i.second << endl;
+	cout << DELIMITER;
+
+	std::map<std::string, std::list<std::string>>dictionary =
+	{
+		{"occasionaly",{"иногда","Время от времени","случайно","изредко"}},
+		{"causes",{"причина","приводить к..."}},
+		{"space",{"космос","пространство","пробел"}},
+		{"void",{"пустота","вакуум","пробел"}},
+		{"sequence",{"последовательность","ряд","эпизод"}},
+		{"consequence",{"следствие","последствие","вывод"}}
+	};
+
+
+#endif
+
+}
